9d590a3523c340644393e8c903fd0e7dd6737e70
Verlã  'Eskurinho Dev <manoads2019@gmail.com>
2021-02-03 20:16:37 -0300

    semana 01 objetos

--- launchBase2.0/aulasJS/index.js
+++ launchBase2.0/aulasJS/index.js
@@ -1,2 +1,25 @@
-const nome  = 'Mano'
-console.log(nome)
+//Criar um programa que calcula a média
+//das notas dos alunos de uma turma
+
+const aluno1 = {
+    nome : "Verlã",
+    nota : 3
+}
+const aluno2 = {
+    nome : "Mayk",
+    nota : 4.8
+}
+const aluno3 = {
+    nome : "Fernanda",
+    nota : 5
+}
+
+
+const media = (aluno1.nota + aluno2.nota + aluno3.nota) / 3
+
+if (media > 5) {
+    console.log(`Turma aprovada com média acima de 5.Parabéns...`)
+}else{
+    console.log(`Turma em recuperação por média abaixo de 5...`)
+}
+console.log(`A média dos alunos da turma é: ${media.toFixed(2)}`)
